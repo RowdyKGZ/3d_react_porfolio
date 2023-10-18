6831b68e433d8e58cb212c13f3b1b05b41c24969
RowdyKGZ <73703199+RowdyKGZ@users.noreply.github.com>
2023-10-18 21:39:06 +0600

    start

---
+++ src/components/Hero.jsx
@@ -0,0 +1,10 @@
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { ComputersCanvas } from "./canvas";
+
+const Hero = () => {
+  return <div>Hero</div>;
+};
+
+export default Hero;
